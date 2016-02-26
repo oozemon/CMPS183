46122ed7949a98358470287013b3d6dd4b9dd470
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2016-02-25 21:36:36 -0800

    destinations schema w/ Country, Region, City

--- models/db_wizard.py
+++ models/db_wizard.py
@@ -40,14 +40,51 @@ db.define_table('past_itinerary',
                 Field('past_it_tips', 'text', length=256),
                 Field('past_it_regrets', 'text', length=256))
 
+"""
+Sketch of a destinations table using the google api
+Google API's place_id is a unique identifier of a place
+We should also consider saving other metadata eg place name, country, etc..
+"""
+"""
 db.define_table('destinations',
                 Field('place_id', unique=True),
                 )
+"""
+
+# TODO distinguish between capital/uncapitalized names
+db.define_table('countries',
+                Field('country_name', unique=True)
+                )
+
+db.define_table('regions',
+                Field('region_name', unique=True)
+                )
+
+db.define_table('cities',
+                Field('city_name', unique=True)
+                )
+
+"""
+Every destination is in a country
+Optionally has a region or city
+
+When determining what the name of the destination is, 
+we should look to see if city is not None. If it is None, then check region.
+If region is none, the destination is a country.
+"""
+# TODO need to ensure that the (des_country, des_region, des_city) tuple is unique
+# TODO support having destinations with cities
+db.define_table('destinations',
+                Field('des_country', 'reference countries', required=True),
+                Field('des_region', 'reference regions'),
+                Field('des_city', 'reference cities')
+                )
 
 db.define_table('images',
-                Field('file', 'upload')
+                Field('img_file', 'upload')
                 )
 
 db.define_table('images_destinations',
-                Field('img', 'reference images')
+                Field('img', 'reference images'),
                 Field('dest', 'reference destinations')
+                )
