e9cb6c82f09565020b8021aed8f6c75c3fa2d852
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2016-03-13 08:21:16 -0700

    change "Index" menu item to "Home"
    Index is a little unusual to user

--- models/menu.py
+++ models/menu.py
@@ -4,7 +4,7 @@ response.meta.author = '%(author)s <%(author_email)s>' % settings
 response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 response.menu = [
-(T('Index'),URL('default','index')==URL(),URL('default','index'),[]),
+(T('Home'),URL('default','index')==URL(),URL('default','index'),[]),
 (T('Destinations'),URL('destinations','index')==URL(),URL('destinations','index'),[]),
 (T('Profile Manage'),URL('default','profile_manage')==URL(),URL('default','profile_manage'),[]),
 ]
